ca7b10f5341d8d30f192e711b338ac88d22e5201
max <max.wi.strange@gmail.com>
2025-03-19 12:15:46 -0400

    initial implementation of server response

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask,render_template,request,session,jsonify
+from markupsafe import escape
+import re
 import random
 import secrets
+
+'''
+developed by strnge
+with assistance from cr0wlet
+03-2025
+'''
+
+#debug types:
+# new_game , bad_guess , good_guess , duplicate_guess
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(8) # tokenize for the session, required
@@ -25,17 +36,34 @@ def find(s, ch):
 # on subsequent calls, searches the word of occurences of the last character guessed, 
 # gets a position list, and replaces those corresponding characters in the display word
 def reveal_word():
-    if not session["display_word"]: 
+    if session["display_word"] == "": # initial generation of display string
         for i in range(len(session["word"])):
             session["display_word"] += "*"
         session["display_word"]
     else:
         reveal_list = find(session["word"], session["graveyard"][-1])
-        word_to_list = list(session["display_word"])
-        for i in range(len(reveal_list)):
-            word_to_list[reveal_list[i]] = session["graveyard"][-1]
-        session["display_word"] = ''.join(word_to_list)
+        if(not reveal_list):# bad guess! health decreased by 1, 
+            session["health"] = session["health"]-1
+            session["debug"] = "bad_guess"
+        else:
+            session["debug"] = "good_guess"
+            word_to_list = list(session["display_word"])
+            for i in range(len(reveal_list)):
+                word_to_list[reveal_list[i]] = session["graveyard"][-1]
+            session["display_word"] = ''.join(word_to_list)
     return
+
+# create json object of the important vars for the game
+def get_state():
+    returnable_json = { 
+                        "display_word": session["display_word"],
+                        "health": session["health"],
+                        "graveyard": session["graveyard"],
+                        "wins": session["wins"],
+                        "losses": session["losses"],
+                        "debug": session["debug"]
+                      }
+    return jsonify(returnable_json)
 
 # welcome screen, leads to gameplay
 @app.route('/')
@@ -49,34 +77,46 @@ def init_game():
     session["wins"] = 0
     session["losses"] = 0
 
-    return new_game()
+    new_game()
+    
+    return render_template('index.html', state=session["debug"], health=session["health"], wins=session["wins"], losses=session["losses"], display=session["display_word"], default_message="Hello!",motd="This is where the MOTD would go")
 
 # sets up a new game still in the same session(lets user continue accruing wins and losses)
 @app.route('/new_game')
 def new_game():
     session["word"] = generate_word() # generate word for the session
+    session["display_word"] = ""
     reveal_word() # initial call to generate display word
-    #session["display_word"] = ""
     session["health"] = 6
-    session["graveyard"] = []
-    session["debug_state"] = "new_game"
-    
-    return render_template('index.html', display_word=session["display_word"], state=session["debug_state"], health=session["health"], wins=session["wins"], losses=session["losses"], graveyard=session["graveyard"])
+    session["graveyard"] = list()
+    session["debug"] = "new_game"
+    # generate json response for the client
+    state = get_state()
+    return state
 
 # client sends user's guess, check against word
 @app.route('/send_guess',methods=['POST'])
 def guess():
-    # TODO: receieve guess from client, verify and send response back
-    request_data = request.args["input_box"]
-    return render_template('index.html', display_word=session["display_word"], state=session["debug_state"], health=session["health"], wins=session["wins"], losses=session["losses"], graveyard=session["graveyard"])
+    request_data = escape(request.form.get("input_box"))
 
+    # regex search to make sure only 1 alpha character was guessed
+    # technically we should only have received this POST req if
+    # the client-side verified that already but we double check in case
+    # there are any issues on the client side
+    if(re.search(r"^[A-Za-z]{1}$", request_data[0]) is not None):
+        if(request_data[0] in session["graveyard"]):# check graveyard to see if this guess has been made already
+            session["debug"] = "duplicate_guess"
+        else:
+            session["graveyard"].append(request_data[0])# add guess to the end of the graveyard
+            reveal_word()# search for the guessed character and modify health/display string as needed
+    return get_state()
 
 # request scoreboard
 @app.route('/scoreboard')
 def scoreboard():
     '''TODO: something like return render_template('index.html', html_file="score_interface.html",'
     ' score_file=local_score_file***json file goes here, declared above***)'''
-
+    # redir makes more sense here
     return -1 
 
 # run localhost only
--- static/app.js
+++ static/app.js
@@ -1,7 +1,14 @@
+/*
+ * developed by strnge
+ * with assistance from cr0wlet
+ * 03-2025
+*/
 var game_data_container = null;
 var display_word = "";
+var display_graveyard = "";
 
 //should prompt if user tries to refresh?
+//TODO: doesn't work right now
 function refresh_prompt(){
     confirm_message = "Are you sure you want to refresh?\nIt will erase your current game session without saving your score.";
     if(confirm(confirm_message)){
@@ -9,104 +16,88 @@ function refresh_prompt(){
     }
 }
 
-function begin_game(){
-    //begin game logic, load game interface
-    display_word = "";
-    window.location.href = '/init';
-}
-
+//begin game logic, load game interface
 function init(){
-    //begin game logic, load game interface
     display_word = "";
     window.location.href = '/init';
-/*     $.ajax({
-        url: '/init', //api route to be called
-        type: 'POST', //request type, POST or GET
-        success: function(response) { //what to do if the server responds successfully
-            //do something
-            window.location.href = '/init'
-        },
-        error: function(error) { //what to do if the server responds with an error
-            console.log(error);
-            update_debug_state('ajax error');
-        }
-    }) */
-}
-/*
-function generate_word(){
-    //reset score
-    text ="";
-    wins = 0;
-    losses = 0;
-    //request word from the server, init display_word and store game_word
-    $.ajax({
-        url: '/init', //api route to be called
-        type: 'POST', //request type, POST or GET
-        data: text,
-        success: function(response) { //what to do if the server responds successfully
-
-        },
-        error: function(error) { //what to do if the server responds with an error
-            console.log(error);
-            update_debug_state('ajax error');
-        }
-    })
 }
 
-function update_game_message(message){
-    document.getElementById("game_message").innerHTML = message;
-}
-
+//updates debug display on page
 function update_debug_state(state){
     document.getElementById('state_debug').innerHTML = "State = ".concat(state);
 }
 
+//updates word display on page
 function update_word_display(){
     document.getElementById('word_container').innerHTML = "Word: ".concat(display_word);
-}*/
+}
+
+function update_winloss(wins, losses){
+    document.getElementById('score_container').innerHTML = "Wins: ".concat(wins) + " || Losses: ".concat(losses);
+}
+
+function update_game_message(debug){
+    var msg_update = ""
+    switch(debug){
+        case "good_guess":
+            msg_update = "Good guess!";
+            break;
+        case "bad_guess":
+            msg_update = "Bad guess!";
+            break;
+        case "duplicate_guess":
+            msg_update = "Please only use english alphabetical characters.";
+            break;
+        case "alpha_err":
+            msg_update = "Duplicate guess!";
+            break;
+        case "length_err":
+            msg_update = "Must be exactly 1 character!";
+            break;
+    }
+    document.getElementById("game_message").innerHTML = msg_update;
+}
+
+function update_graveyard(graveyard){
+    display_graveyard = "";
+    for (var char in graveyard){
+        display_graveyard += char;
+        display_graveyard += " ";
+    }
+    document.getElementById('graveyard').innerHTML = "Graveyard = ".concat(display_graveyard);
+}
 
 // TODO: update this with an AJAX req to handle game logic accordingly
 function submit_guess() {
     var guess = document.getElementById('input_box').value; //grab value from user input box
-    var alphaCheck = /^[A-Za-z]+$/.test(guess); //checks whether the string only contains alphabetical characters
+    var alphaCheck = /^[A-Za-z]{1}$/.test(guess); //checks whether the string only contains alphabetical characters
     if (guess.length != 1){ //checking the length of the char
-        update_game_message("Must be exactly 1 character!");
+        update_game_message("length_err");
         update_debug_state("length_err");
     } else if (!alphaCheck){ //only if guess does not contain only alphabetical characters
-        update_game_message("Please only use english alphabetical characters.");
+        update_game_message("alpha_err");
         update_debug_state("alpha_err");
-    } else { //call to search for char/update string
-        //char_graveyard.push(guess);
-        var response = fetch("/send_guess", {
-            method: 'POST',
-            //TODO: maybe solution?
-        })
+    } else { 
+        try{
+            guess = guess.toLowerCase();
+            fetch("/send_guess", { // send a POST request with our guess, and receive back the game state as a response 
+                method: 'POST',
+                body: guess
+            }).then(function(response){ // we receieve the promise object and turn it into json with .json() 
+                return response.json();
+            }).then(function(json){ // now we can use the fields within the json obj
+                if(json.debug != "game_over" && json.debug != "game_win") {
+                    display_word = json.display_word;
+                    update_word_display();
+                    update_debug_state(json.debug);
+                    update_game_message(json.debug);
+                    update_winloss(json.wins, json.losses);
+                    update_graveyard(json.graveyard);
+                }
+            });
+        } catch (err){
+            console.log(err);
+        }
     }
     document.getElementById("input_box").value = ""; //reset input box to prepare for new guess
 }
-/*
-function game_search(e) {
-    var char_array = [...display_word];//convert these to arrays for ease of individual searching
-    var game_array = [...game_word];
-    var chars_found = 0;
-    for (i = 0;i<game_array.length;i++) {
-        if(game_array[i] == e){
-            char_array[i] = e; // at each index i replace a '*' with element e
-            chars_found++;
-        }
-    }
-    display_word = char_array.toString().replaceAll(/,/gi,""); //convert array back to string
-    if(chars_found>0){
-        update_game_message("Great guess!");
-        update_debug_state("guess_success");
-    } else {
-        update_game_message("Try again!");
-        update_debug_state("guess_fail");
-    }
-    update_word_display();
-    if(display_word === game_word){
-        wins++;
-        update_game_message("Well done! The word was " + game_word);
-        update_debug_state("win");
-    }
-}*/
